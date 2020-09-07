a40c424e2d82ea62c516cc8c43a2d0ba8e2282a3
emacle <qkbeyond@gmail.com>
2020-09-07 17:51:13 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/helloworld1">helloworld1</router-link>
+    <router-link to="/paper">Paper</router-link>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
